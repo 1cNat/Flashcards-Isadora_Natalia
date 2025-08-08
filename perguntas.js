1ffbee1e5c906d005c30e9eab09eac8762530965
1cNat <natalia.dias.goncalves@escola.pr.gov.br>
2025-08-08 12:09:00 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Astronomia', 
+    'A que se refere um núcleo galáctico?', 
+    'Quando o buraco negro central está em intensa atividade'  
+)
+
+criaCartao(
+    'Língua Portuguesa',
+    'Quais palavras são sinônimos de eloquente?', 
+    'Persuasivo,convincente' 
+)
+
+criaCartao(
+    'Física', 
+    'O que é MRUV?', 
+    'Movimento Retilíneo Uniformemente Variado' 
+)
+
+criaCartao(
+    'Arte', 
+    'Qual foi o impacto da Semana de Arte Moderna?', 
+    'Desprender a arte brasileira dos padrões da arte europeia' 
+)
